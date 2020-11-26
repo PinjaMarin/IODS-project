2811c78d8b4799401f75cd5baec82c879c7e2920
Pinja Marin <>
2020-11-27 01:04:20 +0200

    fix line break

--- data/create_human.R
+++ data/create_human.R
@@ -1,5 +1,6 @@
 #Pinja Marin
-#creating the human data through combining two datasets, and making some modifications to the data
+#creating the human data through combining two datasets, and making some modifications 
+#to the data
 
 #Read the “Human development” (hd) and “Gender inequality” (gi) datas into R
 
@@ -8,8 +9,8 @@ hd <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_22
 gi <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/gender_inequality.csv", stringsAsFactors = F, na.strings = "..")
 
 #More information about the datasets:
-http://hdr.undp.org/en/content/human-development-index-hdi # metadata
-http://hdr.undp.org/sites/default/files/hdr2015_technical_notes.pdf #technical information
+# http://hdr.undp.org/en/content/human-development-index-hdi # metadata
+# http://hdr.undp.org/sites/default/files/hdr2015_technical_notes.pdf #technical information
 
 #Exploring the datasets
 dim(hd) # 195 obs, 8 var
@@ -44,7 +45,8 @@ glimpse(gi_f) #seems good.
 
 #Mutating the “Gender inequality” data to create two new variables. 
 
-#First, creating the ratio of Female and Male populations with secondary education in each country.
+#First, creating the ratio of Female and Male populations with secondary education 
+#in each country.
 gi_f <- gi_f %>%
   group_by(country) %>%
   mutate(edu2_ftom = edu2_f / edu2_m) %>%
@@ -72,7 +74,8 @@ write.csv(human, file='human.csv')
 
 
 #continuing data wrangling during week5
-# I am using the data I created last week but the combined data is also available here: http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human1.txt
+# I am using the data I created last week but the combined data is also available here: 
+# http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human1.txt
 
 #load data into R
 human2 <- read.csv("human.csv")
@@ -81,10 +84,17 @@ glimpse(human2)
 head(human2,4) #there is an additional x-column of row numbers, let's delete it.
 human2 <- human2[, 2:20]
 dim(human2) #195,19
-str(human2) # there are 19 variables. Except for the country variable, all variables give numerical information about the coutries (and areas) on matters such as equality and development. The variable GNI (Gross National Income per capita) appears to be in a character form although it is supposed to be a numerical value. Besides GNI, some example variables are "life_exp" = Life expectancy at birth, and "Edu2_f" = Proportion of females with at least secondary education. 
+str(human2) # there are 19 variables. Except for the country variable, all variables 
+#give numerical information about the coutries (and areas) on matters such as 
+#equality and development. The variable GNI (Gross National Income per capita) 
+#appears to be in a character form although it is supposed to be a numerical value. 
+#Besides GNI, some example variables are "life_exp" = Life expectancy at birth, 
+#and "Edu2_f" = Proportion of females with at least secondary education. 
 
-human2$country #this variable mainly refers to different countries but there also some larger areas such as "Europe and Central Asia" and even "World". 
-#More information about the dataset can be found earlier in this script, in the two links from last week.
+human2$country #this variable mainly refers to different countries but there also 
+#some larger areas such as "Europe and Central Asia" and even "World". 
+#More information about the dataset can be found earlier in this script, in the two 
+#links from last week.
 
 #Let's transfrom the GNI to numeric.
 # first, accessing the stringr package
@@ -98,15 +108,17 @@ human2$GNI <- str_replace(human2$GNI, pattern=",", replace ="") %>% as.numeric()
 str(human2$GNI) #seems good.
 
 #Let's exclude unneeded variables and keep only these:
-keep <- c("country", "edu2_ftom", "labour_ftom", "Edu_exp", "life_exp", "GNI", "mat_mor", "Adol_birth", "parl_f")
+keep <- c("country", "edu2_ftom", "labour_ftom", "Edu_exp", "life_exp", "GNI", 
+          "mat_mor", "Adol_birth", "parl_f")
 
 # selecting the 'keep' columns
 human_s <- select(human2, one_of(keep))
 dim(human_s) #9 variables, as wanted
 
 #Let's remove all rows with missing values
-#
-# first, printing out the data along with a completeness indicator as the last column (it shows whether there are any missing values per row)
+
+# first, printing out the data along with a completeness indicator as the last column 
+# (it shows whether there are any missing values per row)
 data.frame(human_s[-1], comp = complete.cases(human_s))
 
 # filtering out the rows with NA values
@@ -114,7 +126,8 @@ human_f <- filter(human_s, complete.cases(human_s))
 dim(human_f) #162 rows now
 
 #Remove the observations which relate to regions instead of countries.
-human_f$country #it seems that the last 7 observations are not countries but regions. Let's remove them. 
+human_f$country #it seems that the last 7 observations are not countries but 
+#regions. Let's remove them. 
 
 # defining the last row we want to keep
 last <- nrow(human_f) - 7
@@ -129,9 +142,11 @@ rownames(humanf) <- humanf$country
 
 #remove the country name column from the data.
 humanf <- select(humanf, -country)
-#
+
 str(humanf) #155 obs, 8 var. This is correct.
 head(humanf,3)
 
-#Save the human data in your data folder including the row names. Let's not overwrite the old ‘human’ data in case there are any issues and instead keep this data as 'humanf'.
+#Save the human data in your data folder including the row names. 
+#Let's not overwrite the old ‘human’ data in case there are any issues and 
+#instead keep this data as 'humanf'.
 write.csv(humanf, file='humanf.csv')
